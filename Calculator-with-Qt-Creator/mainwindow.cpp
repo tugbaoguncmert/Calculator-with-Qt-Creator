dc1bb2b7c81cf7d5bea6119d7c2e5dce7da7610d
tugbaoguncmert <tugba.oguncmert@gmail.com>
2021-04-20 07:03:42 +0300

    Signal and slot of numbers.

--- Calculator-with-Qt-Creator/mainwindow.cpp
+++ Calculator-with-Qt-Creator/mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //signal and slot
+    connect(ui->pushButton_0,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_1,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_2,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_3,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_4,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_5,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_6,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_7,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_8,SIGNAL(clicked()),this,SLOT(digit_pressed()));
+    connect(ui->pushButton_9,SIGNAL(clicked()),this,SLOT(digit_pressed()));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +26,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//Slot Funktion
+void MainWindow::digit_pressed()
+{
+    qDebug()<<"Funktion wird aktiviert";
+
+    QPushButton * button = (QPushButton*) sender();
+
+    double labelnumber = (ui->label->text() + button->text()).toDouble();
+    QString newlabel = QString::number(labelnumber);
+
+    ui->label->setText(newlabel);
+
+    //Printing one by one
+    //ui->label->setText(button->text());
+}
--- Calculator-with-Qt-Creator/mainwindow.h
+++ Calculator-with-Qt-Creator/mainwindow.h
@@ -17,5 +17,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+
+private slots:
+    void digit_pressed();
 };
 #endif // MAINWINDOW_H
